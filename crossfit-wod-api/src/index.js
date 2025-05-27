2b042f3380ea2c07036db096dc6b21f0c21988ce
Hastycode Andreh <hastycodeandreh@gmail.com>
2025-05-27 12:15:13 +0300

    crud added

--- crossfit-wod-api/src/index.js
+++ crossfit-wod-api/src/index.js
@@ -1,12 +1,17 @@
 
+// import express from "express";
 const express = require("express");
 
-
+// import { json } from "body-parser";
+const bodyParser = require("body-parser");
+// import v1WorkoutRouter from "./v1/routes/workoutRoutes";
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
--- crossfit-wod-api/src/v1/routes/workoutRoutes.js
+++ crossfit-wod-api/src/v1/routes/workoutRoutes.js
@@ -3,7 +3,6 @@ const express = require("express");
 const workoutController = require("../../controllers/workoutController");
 
 
-
 const router = express.Router();
 
 router.get("/", workoutController.getAllWorkouts);
